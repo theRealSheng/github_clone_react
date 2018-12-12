728fddebe16548763df8cd22ee227f2bed8367c1
theRealSheng <sheng.huchai@gmail.com>
2018-12-12 22:20:33 +0100

    Change profile from function component to pureComponent

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,19 +1,21 @@
 import './Profile.css';
 import React from 'react';
 
-const Profile = () => {
-  return (
-    <div className="profile">
-      <div>
-        <img className="profile-pic"
-          alt="theRealSheng"
-          src="https://avatars1.githubusercontent.com/u/33520357?v=4" />
+class Profile extends React.PureComponent {
+  render() {
+    return (
+      <div className="profile">
+        <div>
+          <img className="profile-pic"
+            alt="theRealSheng"
+            src="https://avatars1.githubusercontent.com/u/33520357?v=4" />
+        </div>
+        <div>
+          <h1>theRealSheng</h1>
+        </div>
       </div>
-      <div>
-        <h1>theRealSheng</h1>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Profile;
